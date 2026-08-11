8d6418021cc105cfe2665d94bbbdcc22ca913f28
Snehil Modi <snehil24558@iiitd.ac.in>
2026-08-11 12:40:54 +0530

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+bool palindrome(string s, int l, int r){
+    while(l<r){
+        if(s[l]!=s[r]) return false;
+        l++;
+        r--;
+    }
+    return true;
+}
+public:
+    bool validPalindrome(string s) {
+        
+        int l = 0, r = s.size()-1;
+        while(l<r){
+            if(s[l]!=s[r]) return palindrome(s, l+1, r) || palindrome(s, l ,r-1);
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
